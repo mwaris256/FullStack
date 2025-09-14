fc0e5942f5e625366e39e6514a92d8f8a1388071
mwaris256 <mwaris256@gmail.com>
2025-09-13 22:40:01 -0400

    latest

---
+++ muzammil.com/frontend/js/logic.js
@@ -0,0 +1,30 @@
+function myFunction() {
+            document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+//this output
+innerHTML //html
+innerText //text, HTML
+//document.write("Hello World!") //HTML
+window.alert("This is JS class"); //alerting box
+//console.log("this is console") //console
+
+//syntax style: keywords
+var a = 10; //= assigning operator + - * /
+let b = 20;
+const c = 30; //constant
+
+// this is a single line comment
+/* this is a multiple line comment 
+*/
+
+//data types 
+let a = 50; // this is a statement
+x , y = 40, 8; 
+
+function add(x, y) {
+    return x + y;
+    document.write("hello" + a) ;  
+}
+
+window.alert("Hello Javascript");
---
+++ muzammil.com/frontend/js/variable.js
@@ -0,0 +1,34 @@
+name = "Muzammil"; //string --- name is variable, muzammil is value = assignment operator
+let a = 20; //number
+const isStudent = true;
+let price = 10.5; // float
+let address = null; //null
+
+
+let car = "tesla"; //varibale declaration --- string
+car = "bmw"; //variable initialization
+
+const gender = "male" // constant variable declaration and initialization
+
+let firstname; //undefined variable
+firstname = "John"; //string variable initialization
+
+let fruit1= "apple", fruit2 = "banana", fruit3 = "mango"; //multiple variable declaration and initialization 
+
+let m = 10; //integer
+
+let b = 20.5; //float
+console.log(a+b);
+let isLoggedIn = true;
+
+//variable naming conventions
+//1 variable name must start with letter or _ or $
+//2. variable name cannot start with number
+//3. variable name cannot contain space
+//4. variable name cannot be a reserved keyword
+//5. variable name is case sensitive
+//6. variable name should be meaningful
+
+let x = 2 + 2; //expression
+let y = x * 3; //expression
+let z = y - x; //expression
